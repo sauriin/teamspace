2710ebc0fe7bd067e8d9772d926087c5346d36ca
sauriin <saurinparmar2324@gmail.com>
2025-06-28 19:17:30 +0530

    Base Auth Done

--- convex/files.js
+++ convex/files.js
@@ -1,13 +1,29 @@
-import { mutation } from "./_generated/server";
-import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
 
 export const createFile = mutation({
   args: {
     name: v.string(),
   },
   async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("You must be logged in to upload file.");
+    }
     await ctx.db.insert("files", {
       name: args.name,
     });
+  },
+});
+
+export const getFiles = query({
+  args: {},
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      return [];
+    }
+    return ctx.db.query("files").collect();
   },
 });
--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,16 @@
 
 import { Button } from "@/components/ui/button";
 import { SignInButton } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
   const createFile = useMutation(api.files.createFile);
+  const getFiles = useQuery(api.files.getFiles);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <SignInButton mode="modal" cursor="pointer" />
         <Button
           onClick={() => {
             createFile({ name: "Hello There!!" });
@@ -19,6 +19,10 @@ export default function Home() {
         >
           Click!!
         </Button>
+
+        {getFiles?.map((file) => {
+          return <div key={file._id}>{file.name}</div>;
+        })}
       </main>
     </div>
   );
